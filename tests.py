8b911a1385755b085222dfc98957c14e57c50ceb
Abdifatah Mohamed <mofaatah@gmail.com>
2024-03-01 00:36:34 -0600

    Added task and updated tests

--- tests.py
+++ tests.py
@@ -1,4 +1,13 @@
 import unittest
+import task
+
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
