c978f22f0b27c54269d57182f3071225977c92cf
Abdifatah Mohamed <mofaatah@gmail.com>
2024-03-01 00:55:27 -0600

    Added test2

--- tests.py
+++ tests.py
@@ -7,10 +7,11 @@ class TestCase(unittest.TestCase):
     def test1(self):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
-    
+
     def test2(self):
         expected = "Hola World"
         self.assertEqual(task.my_func(), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
